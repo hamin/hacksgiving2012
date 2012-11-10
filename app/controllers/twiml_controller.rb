06155387bbb11e0776169316b46fa454ff58c86b
Haris Amin <aminharis7@gmail.com>
2012-11-10 04:03:30 -0500

    testing twiml for voice callback

--- app/controllers/twiml_controller.rb
+++ app/controllers/twiml_controller.rb
@@ -3,15 +3,6 @@ class TwimlController < ApplicationController
 
 	def voice
 		# @client = Twilio::REST::Client.new(TWILIO_SID, TWILIO_AUTH_TOKEN)
-
-		# build up a response
-		response = Twilio::TwiML::Response.new do |r|
-		  r.Say 'hello there', :voice => 'woman'
-		end
-
-		respond_to do |format|
-      format.xml { render :xml => response.text }
-    end
 	end
 
 	def sms
