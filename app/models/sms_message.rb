c864f9841aa942d5d05f4cf65bb7ca4d6987f620
Raquel Hernandez <maggit@maggit.net>
2012-11-10 16:41:49 -0500

    Ui changes to sms widget

--- app/models/sms_message.rb
+++ app/models/sms_message.rb
@@ -1,5 +1,5 @@
 class SmsMessage < ActiveRecord::Base
-  attr_accessible :body, :from_num, :seen, :to_num, :user_id, :seen, :seen_by_id
+  attr_accessible :body, :from_num, :seen, :to_num, :user_id, :seen, :seen_by_id, :campaign_id
 
   belongs_to :user
   belongs_to :seen_by, :class_name => 'User', :foreign_key => 'seen_by_id'
