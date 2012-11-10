f307d6e2e57298efdf19bd63aa95ea82987187a0
Raquel Hernandez <maggit@maggit.net>
2012-11-10 00:59:44 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hacksgiving2012::Application.config.secret_token = '2917cb01289091d6ab012c2fbf889bb7491d8ec997abd81f82a0ac0493a67f75ba0d819c62de04c1f3f33bc1cfd45115440b5056d2acc401b3753ca517620cf9'
