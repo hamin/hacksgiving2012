b1b38d3b6b2672dad6c7ad56b8651549d07687df
Raquel Hernandez <maggit@maggit.net>
2012-11-10 01:54:03 -0500

    Adding divise for user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Hacksgiving2012::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110063122) do
+ActiveRecord::Schema.define(:version => 20121110064524) do
 
   create_table "notes", :force => true do |t|
     t.text     "body"
@@ -31,6 +31,25 @@ ActiveRecord::Schema.define(:version => 20121110063122) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "phone_number",                           :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "voice_messages", :force => true do |t|
     t.string   "from_num"
