192e886eb3c71ab1ee259285b06714c8d8be94b4
Raquel Hernandez <maggit@maggit.net>
2012-11-10 14:08:11 -0500

    Cleaning up js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,26 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui-1.8.21.custom.min
 //= require twitter/bootstrap
-//= require_tree .
+//= require jquery.cookie
+//= require jquery.dataTables.min
+//= require fullcalendar.min
+//= require excanvas
+//= require jquery.chosen.min
+//= require jquery.uniform.min
+//= require jquery.cleditor.min
+//= require jquery.noty
+//= require jquery.elfinder.min
+//= require jquery.raty.min
+//= require jquery.iphone.toggle
+//= require jquery.uploadify-3.1.min
+//= require jquery.gritter.min
+//= require jquery.imagesloaded
+//= require jquery.masonry.min
+//= require jquery.knob
+//= require jquery.sparkline.min
+//= require bootstrap
+//= require custom
+//= require excanvas
+//= require home
