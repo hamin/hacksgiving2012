c3009dc0b5866c9e881e2c20a92256237243c50b
Haris Amin <aminharis7@gmail.com>
2012-11-10 04:11:53 -0500

    fixing bug

--- app/controllers/twiml_controller.rb
+++ app/controllers/twiml_controller.rb
@@ -1,8 +1,10 @@
 class TwimlController < ApplicationController
+	protect_from_forgery, :except => [:voice]
 	skip_before_filter :verify_authenticity_token
 
 	def voice
 		# @client = Twilio::REST::Client.new(TWILIO_SID, TWILIO_AUTH_TOKEN)
+		render 'voice.xml.erb', :content_type => 'text/xml'
 	end
 
 	def sms
